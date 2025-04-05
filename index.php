534a4e061849a8b676c0856ee5cd0519392eb19a
Christoph Daum <christoph.daum@coding-pioneers.com>
2025-04-05 09:54:40 +0200

    refactor: phpcbf - improve code readability and formatting in multiple template files

--- index.php
+++ index.php
@@ -19,7 +19,10 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				?>
 
 				<?php
 					/* Include the Post-Format-specific template for the content.

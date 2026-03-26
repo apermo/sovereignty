5b19a408644412640e982ed96a6d83fd83a0cecb
Christoph Daum <christoph.daum@coding-pioneers.com>
2026-03-26 08:12:18 +0100

    fix: remove wp_body_open() compat shim
    wp_body_open() exists since WP 5.2, the
    function_exists check is unnecessary.
    
    Closes #54

--- header.php
+++ header.php
@@ -24,21 +24,7 @@ use Apermo\Sovereignty\Template\Tags;
 
 <body <?php body_class(); ?><?php Semantics::output( 'body' ); ?>>
 <?php
-/**
- * Fire the wp_body_open action for theme compatibility.
- *
- * @see https://make.wordpress.org/core/2019/04/24/miscellaneous-developer-updates-in-5-2/
- */
-if ( function_exists( 'wp_body_open' ) ) {
-	wp_body_open();
-} else {
-	/**
-	 * Fires when the template calls for the wp_body_open action.
-	 *
-	 * @since WordPress 5.2.0
-	 */
-	do_action( 'wp_body_open' ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound -- WP core hook.
-}
+wp_body_open();
 ?>
 <div id="page">
 	<div class="skip-link screen-reader-text"><a href="#primary" title="<?php esc_attr_e( 'Skip to content', 'sovereignty' ); ?>"><?php esc_html_e( 'Skip to content', 'sovereignty' ); ?></a></div>
